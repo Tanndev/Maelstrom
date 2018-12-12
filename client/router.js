a29ff9a70aeed2ea3399226b1c621152da3d1b42
James Tanner <james.tanner@tanndev.com>
2018-12-12 10:19:39 -0500

    Add outline for character sheets

--- client/router.js
+++ client/router.js
@@ -1,4 +1,6 @@
+const createError = require('http-errors');
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 
@@ -27,6 +29,16 @@ router.get('/documentation/:document', (req, res, next) => {
     } else next();
 });
 router.use('/documentation', express.static(path.join(__dirname, 'documentation')));
+
+// Serve character sheets.
+router.get('/character/:id?', (req, res, next) => {
+    let characterId = req.params.id || 'demo';
+    fs.readFile(path.join(__dirname, '..', 'characters', `${characterId}.json`), 'utf8', (error, contents) => {
+        if (error) return next(createError(404));
+        res.locals.character = JSON.parse(contents);
+        res.render('character-sheet');
+    });
+});
 
 // Export the router.
 module.exports = router;
