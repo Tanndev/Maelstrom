295f05ea354908a5308ead3f4fcd018e3309f590
James Tanner <james.tanner@tanndev.com>
2018-12-25 01:24:11 -0500

    ci(semantic-release): use javascript file for release config

---
+++ release.config.js
@@ -0,0 +1,86 @@
+const COMMIT_ANALYZER = [
+    '@semantic-release/commit-analyzer',
+    {
+        preset: 'angular',
+        releaseRules: [
+            {type: 'feat', release: 'minor'},
+            {type: 'fix', release: 'patch'},
+            {type: 'perf', release: 'patch'},
+            {type: 'revert', release: 'patch'},
+            {type: 'docs', scope: 'rules', release: 'minor'},
+            {type: 'docs', scope: 'lore', release: 'patch'},
+            {type: 'style', scope: 'rules', release: 'patch'},
+            {type: 'style', scope: 'website', release: 'patch'},
+            {type: 'style', scope: '/character.*/', release: 'minor'},
+            {type: 'ci', release: 'patch'},
+            {type: 'rules', release: 'minor'}
+        ]
+    }
+];
+
+const RELEASE_NOTES_GENERATOR = [
+    '@semantic-release/release-notes-generator',
+    {
+        preset: 'angular',
+        writerOpts: {
+            "transform": {
+                "type": function (type) {
+                    switch (type) {
+                        case "feat":
+                            return "Features";
+                        case "fix":
+                            return "Fixes";
+                        case "perf":
+                            return "Performance Improvements";
+                        case "revert":
+                            return "Reverts";
+                        case "docs":
+                            return "Documentation";
+                        case "style":
+                            return "Styles";
+                        case "refactor":
+                            return "Refactoring";
+                        case "test":
+                            return "Tests";
+                        case "build":
+                            return "Build System";
+                        case "ci":
+                            return "Continuous Integration";
+                        case "rules":
+                            return "Rule Changes";
+                        default:
+                            return type;
+                    }
+                }
+            }
+        }
+    }
+];
+
+const NPM = '@semantic-release/npm';
+
+const GITHUB = [
+    '@semantic-release/github',
+    {
+        successComment: "This ${issue.pull_request ? 'pull request is included' : 'issue is fixed'} in version ${nextRelease.version}",
+        failTitle: 'CI: The automatic release failed',
+        labels: ['release pipeline']
+    }
+];
+
+const DOCKER = [
+    'semantic-release-docker',
+    {name: 'jftanner/maelstrom'}
+];
+
+module.exports = {
+    branch: 'master',
+    repositoryUrl: 'git@github.com:Tanndev/Maelstrom.git',
+    plugins: [
+        COMMIT_ANALYZER,
+        RELEASE_NOTES_GENERATOR,
+        NPM,
+        GITHUB,
+        DOCKER
+    ]
+};
