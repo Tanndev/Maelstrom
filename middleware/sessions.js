b63c7951495ffbd662e52c95daeab491b3d6a68d
James Tanner <james.tanner@tanndev.com>
2018-12-30 22:31:06 -0500

    feat(accounts): automatically switch between Redis and MemoryStore

--- middleware/sessions.js
+++ middleware/sessions.js
@@ -2,18 +2,31 @@ const session = require('express-session');
 const redis = require('redis');
 const RedisStore = require('connect-redis')(session);
 
-let clientReady = false;
-
-const client = redis.createClient({host: process.env['REDIS_HOST'], port: process.env['REDIS_PORT']});
-client.on('ready', () => {
-    clientReady = true;
-});
-
-module.exports = session({
+const DEFAULT_SETTINGS = {
     name: "maelstrom",
     resave: false,
     saveUninitialized: false,
     secret: "Temporary Secret", // TODO Generate or load a secure secret.
-    store: new RedisStore({client, logErrors: true}),
     unset: 'destroy'
+};
+
+// Set up the middleware.
+const client = redis.createClient({host: process.env['REDIS_HOST'], port: process.env['REDIS_PORT']});
+const memorySessionMiddleware = session({...DEFAULT_SETTINGS});
+const redisSessionMiddleware = session({store: new RedisStore({client}), ...DEFAULT_SETTINGS});
+let currentMiddleware = memorySessionMiddleware;
+
+// Switch between middleware on Redis client events.
+client.on('ready', () => {
+    console.log('Connected to Redis service.');
+    currentMiddleware = redisSessionMiddleware;
 });
+client.on('end', () => {
+    console.error("Redis service disconnected. Falling back to MemoryStore.");
+    currentMiddleware = memorySessionMiddleware;
+});
+
+// Export a wrapper around the session middleware.
+module.exports = (req, res, next) => {
+    currentMiddleware(req, res, next);
+};
